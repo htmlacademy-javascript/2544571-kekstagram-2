71b050766be9f2861f30e0dee92aacf260448e03
cooperfirewalk <cooperfirewalk@gmail.com>
2024-12-17 21:40:25 +0300

    проверяет работу github

--- js/main.js
+++ js/main.js
@@ -3,9 +3,3 @@ import './miniatures.js';
 
 //Импорт модуля с отрисовкой полноразмерных изображений
 import './fullscale.js';
-
-//Импорт функции, формирующей фотографии
-import {createPhotosArray} from './data.js';
-
-//Вызов функции, формирующей фотографии
-createPhotosArray();
