c83c68f147d9a99f8f3e516f7ac4d12b4349bc0d
cooperfirewalk <cooperfirewalk@gmail.com>
2024-12-18 04:06:11 +0300

    выполняет задание 8.2

--- js/fullscale.js
+++ js/fullscale.js
@@ -13,12 +13,14 @@ const bigPictureCommentsCount = bigPicture.querySelector('.social__comment-total
 const commentContainer = bigPicture.querySelector('.social__comments');
 const shownCommentsCount = bigPicture.querySelector('.social__comment-shown-count');
 const bigPictureDescription = bigPicture.querySelector('.social__caption');
-const socialCommentCount = bigPicture.querySelector('.social__comment-count');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
+
+const COMMENTS_PACE = 5; // Константа, задающая шаг - количество показываемых комментариев
 
 // Создаем шаблон для добавления комментариев
 const commentTemplate = document.createElement('li');
 commentTemplate.classList.add('social__comment');
+commentTemplate.classList.add('hidden'); // генерируем сразу скрытые комментарии
 commentTemplate.innerHTML = `<img
     class="social__picture"
     src="{{аватар}}"
@@ -29,15 +31,52 @@ commentTemplate.innerHTML = `<img
 // Создаем фрагмент, куда будем добавлять комментарии
 const genCommentsFragment = document.createDocumentFragment();
 
-// Функция для подсчета количества показанных комментариев к фотографии
-const getShownComments = () => {
-  shownCommentsCount.textContent = commentContainer.querySelectorAll('li').length - commentContainer.querySelectorAll('li[class="social__comment hidden"]').length;
+// Функция для подсчета количества комментариев
+
+const getCommentsInfo = () => {
+  const commentsList = commentContainer.querySelectorAll('.social__comment');
+  const commentsTotal = commentsList.length;
+  const commentsHidden = commentContainer.querySelectorAll('li[class="social__comment hidden"]').length;
+  const commentsActive = commentsTotal - commentsHidden;
+  return { total: commentsTotal, active: commentsActive, hidden: commentsHidden, array: commentsList };
 };
+
+// Функция для вывода количества показанных комментариев к фотографии
+const getShownComments = () => {
+  const comments = getCommentsInfo();
+  shownCommentsCount.textContent = comments.active;
+};
+
+// Функция для прохода по массиву и открытия комментариев
+
+const commentsOpen = (nodeList, start, end) => {
+  for (let i = start; i < end; i++) {
+    nodeList[i].classList.remove('hidden');
+  }
+};
+
+// Функция для показа первых комментариев и кнопки подгрузки
+
+const getCommentsStack = () => {
+  const comments = getCommentsInfo();
+  const caseParam = true;
+  switch (caseParam) {
+    case (comments.hidden === 0): commentsLoader.classList.add('hidden'); break;
+    case (comments.hidden <= COMMENTS_PACE):
+      commentsLoader.classList.add('hidden');
+      commentsOpen(comments.array, comments.active, comments.active + comments.hidden);
+      break;
+    case (comments.hidden > COMMENTS_PACE):
+      commentsOpen(comments.array, comments.active, comments.active + COMMENTS_PACE);
+      break;
+  }
+  getShownComments();
+};
+
 
 // Функция, создающая комментарии к фотографии из данных из массива
 const genComments = (dataArray) => {
   dataArray.forEach(({ avatar, message, name }) => {
-    // const commentElement = comment.cloneNode(true);
     const commentElement = commentTemplate.cloneNode(true);
     commentElement.querySelector('.social__picture').src = avatar;
     commentElement.querySelector('.social__picture').alt = name;
@@ -75,6 +114,7 @@ function closeBigPicture() {
 // Добавляем событие на кнопку закрытия
 bigPictureCloseButton.addEventListener('click', () => {
   closeBigPicture();
+  commentsLoader.classList.remove('hidden'); // Открываем кнопку "загрузить еще" (это вариант по умолчанию)
 });
 
 // Функция для обработчика события по клику на миниатюру
@@ -88,15 +128,14 @@ function onMiniatureClick(evt) {
     bigPictureCommentsCount.textContent = elemData.comment.length;
     bigPictureDescription.textContent = elemData.description;
     genComments(elemData.comment);
-    getShownComments();
+    getCommentsStack();
     openBigPicture();
-
-    // Скрываем счетчик комментариев и кнопку "загрузить еще" (по условиям задания)
-    socialCommentCount.classList.add('hidden');
-    commentsLoader.classList.add('hidden');
   }
 }
 
 // Добавляем событие на миниатюры (родительский элемент)
 picturesContainer.addEventListener('click', onMiniatureClick);
+
+// Добавляем событие на кнопку подгрузки комментариев
+commentsLoader.addEventListener('click', getCommentsStack);
 
